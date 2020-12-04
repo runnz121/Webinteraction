c6d904c1418e0cf25318d95afe2a43054e816b03
runnz <runnz121@gmail.com>
2020-12-04 23:55:27 +0900

    update

--- js/1.js
+++ js/1.js
@@ -18,7 +18,14 @@
             heightNum: 5,        //브라우저 높이의 5배로 scrollHeight 새팅
             scrollHeight : 0,    //각 구간의 스크롤 높이의 정보를 저장하기위한 변수
             objs: {              //HTML 객체를 모아놓는 객체를 만듬 각 section의 ID를 저장
-                container: document.querySelector('#scroll-section-0')
+                container: document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+                messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+                messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+                messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+            },
+            values : {//message에 어떤값을 줄지 전달
+                messageA_opacity: [0, 1] //messageA 투명도 조절 투명도가 0 ->1로 변경
             }
         },
 
@@ -65,10 +72,57 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight; //윈도우 창 비례해서 적용시키기 위해
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`; //${}내부에는 변수를 쓸 수 있다. Contanier 에 각각 scrollheight를 적용
         }   //왼쪽에 오른쪽 값을 넣어준다.
+
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0;
+        for (let i = 0; i<sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight; //각 scene의 scrollheignht를 더해서 넣어주고있다.
+            if(totalScrollHeight >= yOffset) {//totalscrollheight값과 현재 scroll위치를 비교해서 현재 스크롤 위치보다 토탈이 커지면
+                currentScene = i; //현재 i를 currentscene로 세팅하고 for문을 멈춘다
+                break;//현재 scene에서 새로고침해도 그 scene 이 표시된다.
+
+            }
+        }
+        document.body.setAttribute(`id`,`show-scene-${currentScene}`);
    }
 
 
 
+   function calcValues(values, currentYOffset) { //위의 message opacity값 계산하는 함수 ,변수로 opacity 값, 현재 스크롤된 값 넣는다
+        let rv;
+        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight; //스크롤된 비율 구하기 //현재씬(스크롤섹션/전체 스크롤 섹션)
+        
+        rv = scrollRatio * (values[1]-values[0]) + values[0]; //초기값을 더해준다. 그래야 전체 값 됨
+        
+        return rv;
+
+
+   }
+
+
+   function playAnimaation() {//애니메이션 조절 함수
+       const objs = sceneInfo[currentScene].objs; //위의 objs 와 values를 변수로 받음
+       const values = sceneInfo[currentScene].values;
+       const currentYOffset = yOffset - prevScrollHeight; //현재 스크롤 위치  - 이전 스크롤 위치 = 해당 신 시작 부터 스크롤된 위치
+        switch(currentScene) { //조건문으로 해당 scene만 play시킴 분기 나눔
+            case 0:
+                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset );
+                objs.messageA.style.opacity = messageA_opacity_in;
+                break;
+
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+        }
+   }
 
 
 
@@ -91,11 +145,17 @@
         if(yOffset < prevScrollHeight) {
             if(currentScene === 0) return; //currentScene 0번째 scene이라면 그냥 종료(브라우저가 바운스 효과로 인해 마이너스 값이 나올 수 있음을 방지)
             currentScene--;
+            document.body.setAttribute(`id`,`show-scene-${currentScene}`);//변수랑 문자열 섞여있으면 ` (백틱) 써야된다
+
         } //스크롤을 위로 올릴때 scene 2->1로 페이지가 올라가는 시점이라고 가정하면
           //yOffset이 prevScrollHeight + 와 current Scene 값 보다 작아지면 scene 번호가 바뀐다는 것을 표현
 
-          console.log(currentScene);
-    }
+         //현재 보여지는 scene을 보여줌 
+
+         playAnimaation();
+
+        
+    }   
    
 
 
@@ -104,6 +164,9 @@
         yOffset = window.pageYOffset; //yoffset을 갱신해줌 편의상 변수로 만듬
         scrollLoop();
     });
+    // window.addEventListener(`DOMContentLoaded`, setLayout);//htML 콘텐트만 로딩되면 바로 시작
+    window.addEventListener('load', setLayout);//모든 이미지 로드하고나서 초기화
+    window.addEventListener('resize', setLayout);
 
 
     setLayout();
